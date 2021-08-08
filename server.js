4b32b8a0f6139c0ba598dc961c3b1dcfb951175e
Ninte <ninte.dev@gmail.com>
2021-08-08 16:55:25 +0100

    Added extra middleware. Sectioned code with comments. Added test api route.

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+
+// extra middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// api routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'Hello API' });
+});
 
-
-
-
+// server mount
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
